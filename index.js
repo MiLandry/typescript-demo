e0a9831b7bdc0ccbb01784514c3b159625eaa002
Michael Landry <mlandryviolin@gmail.com>
2018-12-15 16:26:35 -0500

    snaothei

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-alert('why');
 //types
-var myName = "mike";
+var myName = "Mike";
 var myAge = 31;
 var canVote = true;
 var anything = "dog";
 anything = 2;
+document.getElementById("tsStuff")
+    .innerHTML = "My name is " + myName;
+document.write("myname is a " + typeof (myName) + "<br />");
+document.write("canVote is a " + typeof (canVote) + "<br />");
+document.write("myname is a " + typeof (myName) + "<br />");
+var strToNum = parseInt("5");
+var superman = {
+    realName: "Clark Kent",
+    superName: "Superman",
+};
+document.write(superman.realName + " is " + superman.superName + " <br />");
--- index.ts
+++ index.ts
@@ -1,11 +1,31 @@
-alert('why')
 
 //types
-var myName: string = "mike";
-
+var myName: string = "Mike";
 var myAge: number = 31;
-
 var canVote: boolean = true;
-
 var anything: any ="dog";
 anything = 2;
+
+document.getElementById("tsStuff")
+    .innerHTML = "My name is " + myName
+
+document.write("myname is a " + typeof(myName) + "<br />")
+document.write("canVote is a " + typeof(canVote) + "<br />")
+document.write("myname is a " + typeof(myName) + "<br />")
+
+var strToNum: number = parseInt("5");
+
+// interfaces
+
+interface SuperHero {
+    realName: String;
+    superName: String;
+}
+
+var superman: SuperHero = {
+    realName: "Clark Kent",
+    superName: "Superman",
+}
+
+document.write(superman.realName + " is " + superman.superName + " <br />")
+
