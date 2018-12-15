dd11404a36694157b8f44cea5e7d3f83cac5cab5
Michael Landry <mlandryviolin@gmail.com>
2018-12-15 16:41:01 -0500

    work

--- index.js
+++ index.js
@@ -15,3 +15,48 @@ var superman = {
     superName: "Superman",
 };
 document.write(superman.realName + " is " + superman.superName + " <br />");
+//array
+var employees = ["Bob", "Alic"];
+var superHeroers;
+//string coersion
+document.write("5 + String 2 = " + (5 + "2")); //52
+document.write('<br />');
+//strang increment stuff
+var randNum = 1;
+document.write("randNum++ = " + randNum++); //1 - it prints then incr
+document.write('<br />');
+document.write("++randNum = " + ++randNum); //3 - it inc then prints
+document.write('<br />');
+// let is different in TS
+// you can't reassign a let in a nested block
+var sampLet = 123;
+if (true) {
+    var sampLet_1 = 456;
+}
+document.write("value of samLet is " + sampLet);
+document.write('<br />');
+var sampvar = 123;
+if (true) {
+    var sampvar = 456;
+}
+document.write("value of samvar is " + sampvar);
+document.write('<br />');
+// for in and for of
+var randArray = [5, 6, 7, 8];
+for (var val in randArray) {
+    document.write(val + "<br />");
+} // prints index
+var strArray = randArray.map(String);
+for (var _i = 0, strArray_1 = strArray; _i < strArray_1.length; _i++) {
+    var val = strArray_1[_i];
+    document.write(val + "<br />"); //print element
+}
+// functions
+var getSum = function (num1, num2) {
+    return num1 + num2;
+};
+var theSum1 = getSum(5, 2);
+// figure out snippets for vscode
+document.write("5 + 2 = ", theSum1 + "<br />");
+//resume at 21:00
+//https://www.youtube.com/watch?v=-PR_XqW9JJU
--- index.ts
+++ index.ts
@@ -29,3 +29,67 @@ var superman: SuperHero = {
 
 document.write(superman.realName + " is " + superman.superName + " <br />")
 
+
+//array
+var employees: string[] = ["Bob", "Alic"]
+var superHeroers: SuperHero[]
+
+
+//string coersion
+document.write("5 + String 2 = " + (5+"2")) //52
+document.write('<br />')
+
+//strang increment stuff
+var randNum : number = 1
+document.write("randNum++ = " + randNum++) //1 - it prints then incr
+document.write('<br />')
+document.write("++randNum = " + ++randNum) //3 - it inc then prints
+document.write('<br />')
+
+// let is different in TS
+// you can't reassign a let in a nested block
+
+let sampLet = 123
+
+if (true {
+    let sampLet = 456
+})
+
+document.write("value of samLet is " + sampLet)
+document.write('<br />')
+
+var sampvar = 123
+
+if (true {
+    var sampvar = 456
+})
+
+document.write("value of samvar is " + sampvar)
+document.write('<br />')
+
+
+// for in and for of
+
+var randArray = [5,6,7,8]
+
+for(var val in randArray) {
+    document.write(val + "<br />")
+} // prints index
+
+var strArray = randArray.map(String)
+
+for(var val of strArray){
+    document.write(val + "<br />") //print element
+}
+
+// functions
+var getSum = function(num1: number, num2: number): number{
+    return num1 + num2
+}
+
+var theSum1: number = getSum(5,2)
+
+// figure out snippets for vscode
+document.write("5 + 2 = " theSum1 + "<br />")
+//resume at 21:00
+//https://www.youtube.com/watch?v=-PR_XqW9JJU
